b0d921599f5e08bdbdbe8594dff07b91ae9556a7
NicolasKurz67 <nicolaskurz67@gmail.com>
2022-08-02 10:11:41 -0400

    Implemento de functions

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <>
+class App extends Component {
+  
+  state = {
+    name: 'Nícolas',
+    cont: 0
+  }
+  
+  mudarNome = ()=>{
+    this.setState({name: 'Kurz', cont: 0});
+  }
+
+  // voltarNome = ()=>{
+  //   const voltarNome = document.getElementById('#voltarNome');
+  //   voltarNome.addEventListener('click', ()=>{
+  //     this.setState({name: 'Nícolas', cont: 0});
+  //   });
+  // }
+
+  increment = ()=>{
+    const {cont} = this.state;
+    this.setState({cont: cont+1});
+  }
+
+  decrement = ()=>{
+    const {cont} = this.state;
+    this.setState({cont: cont-1});
+  }
+  
+
+  render() {
+
+    const {name, cont} = this.state;
+
+    return (
       <div className="MyApp">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello world!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p  onClick={this.mudarNome}>
+            {name} {cont}
+          </p>
+          {/* <button id='voltarNome' type="button" class="btn btn-outline-info btn_met" onClick={this.voltarNome}>
+            Reset name
+          </button> */}
+          <button type="button" class="btn btn-outline-info btn_met" onClick={this.increment}>
+            Value Up
+          </button>
+          <button type="button" class="btn btn-outline-info btn_met" onClick={this.decrement}>
+            Value Down
+          </button>
         </header>
       </div>
-
-      <div>
-        Olá mundo
-      </div>
-    </>
-  );
+    );  
+  }
 }
+
 
 export default App;
